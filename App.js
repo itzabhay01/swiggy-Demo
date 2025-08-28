59044a89e2284fe0742435206d6230409a0d0d3b
itzabhay01 <abhaykumar_ee21b6_19@dtu.ac.in>
2025-08-29 02:53:50 +0530

    done some changes

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import React from "react"
 import {createRoot} from "react-dom/client"
 
 const AppLayout = ()=>{
-
+    return <h1>Namaste React</h1>
 }
 const root = createRoot(document.getElementById("root"))
+console.log(root)
 root.render(<AppLayout/>)
